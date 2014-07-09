a43977a1b73b1984cc1fd3afd215288c00a45d69
Chase Moody <chase.moody@mutualmobile.com>
2014-07-09 10:42:50 -0500

    Added d3 chart

--- www/js/calculatorCtrl.js
+++ www/js/calculatorCtrl.js
@@ -1,7 +1,3 @@
-// ToDo
-// Add timestamp
-// Add geolocation
-
 angular.module('app.calc.controllers', [])
 
 .controller('CalculatorCtrl', function($scope) {
@@ -25,19 +21,32 @@ angular.module('app.calc.controllers', [])
 		$scope.data.total = (parseFloat($scope.data.bill) + parseFloat($scope.data.tip)).toFixed(2);
 	};
 
-	$scope.submitExpense = function() {
-		if(localStorage['expenses']) {
-			var expenses = JSON.parse(localStorage['expenses']);
-
-			expenses.push($scope.data);
-
-			localStorage.setItem('expenses', JSON.stringify(expenses));
-		}else{
-			localStorage.setItem('expenses', JSON.stringify([$scope.data]));
+	$scope.submitExpense = function(tip) {
+		// Check if tip or no tip
+		if(!tip) {
+			$scope.data.tip = 0;
 		}
 
-		$scope.setdefaults();
-		$scope.activeToggle('ten', 0.10);
+		navigator.geolocation.getCurrentPosition(function(position) {
+			$scope.data.date = new Date();
+			$scope.data.coordinates = {
+				latitude: position.coords.latitude, 
+				longitude: position.coords.longitude
+			};
+
+			if(localStorage['expenses']) {
+				var expenses = JSON.parse(localStorage['expenses']);
+
+				expenses.push($scope.data);
+
+				localStorage.setItem('expenses', JSON.stringify(expenses));
+			}else{
+				localStorage.setItem('expenses', JSON.stringify([$scope.data]));
+			}
+
+			$scope.setdefaults();
+			$scope.activeToggle('ten', 0.10);
+		});
 	};
 
 	$scope.activeToggle = function(active, percent) {
@@ -61,6 +70,61 @@ angular.module('app.calc.controllers', [])
 })
 
 .controller('StatsCtrl', function($scope) {
+	$scope.expenses = JSON.parse(localStorage.getItem('expenses'));
+
+	// Loop through expenses
+	// var expenses
+    $scope.data = {
+    	bill: 0,
+	    tip: 0,
+	    total: 0,
+	    avg: (($scope.data.tip / $scope.data.total)*100).toFixed(0)
+    };
+
+    angular.forEach($scope.expenses, function(value, key) {
+    	this.bill += parseFloat(value.bill);
+        this.tip += parseFloat(value.tip);
+        this.total += parseFloat(value.total);
+    }, $scope.data);
+
+	var w = 200;                        //width
+    var h = 200;                        //height
+    var r = Math.min(w, h) / 2;   		//radius
+    var color = d3.scale.ordinal().range(["#ff0", "rgb(0, 178, 89)"]);
+    var data = [{"value": $scope.data.tip},{"value": $scope.data.bill}];
+
+    var vis = d3.select(document.querySelector('#pieChart'))
+        .append("svg:svg")              //create the SVG element inside the <body>
+        .data([data])                   //associate our data with the document
+        .attr("width", w)           	//set the width and height of our visualization (these will be attributes of the <svg> tag
+        .attr("height", h)
+        .append("svg:g")                //make a group to hold our pie chart
+        .attr("transform", "translate(" + r + "," + r + ")");    //move the center of the pie chart from 0, 0 to radius, radius
+
+    var arc = d3.svg.arc()   //this will create <path> elements for us using arc data
+        .outerRadius(r)
+        .innerRadius(r - 40);
+
+    var pie = d3.layout.pie()           			//this will create arc data for us given a list of values
+    	.sort(null)
+        .value(function(d) { return d.value; });    //we must tell it out to access the value of each element in our data array
+
+    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
+        .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+        .append("svg:g")                    //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+        .attr("class", "slice");            //allow us to style things in the slices (like text)
+
+    arcs.append("svg:path")
+        .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
+        .transition()
+        .duration(2000)
+        .attrTween("d", tweenPie);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+
+    function tweenPie(b) {
+    	var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+    	return function(t) { return arc(i(t)); };
+    }
 
 })
 
